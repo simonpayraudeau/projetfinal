2b4ed21919e752a69598b29589bc10bbc5b5c919
Simon Payraudeau <payraudeau.simon@orange.fr>
2020-10-01 17:16:07 +0200

    voyage voyage #LaurentVoulzy

--- src/Entity/Voyage.php
+++ src/Entity/Voyage.php
@@ -3,8 +3,32 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiSubresource;
+
+/**
+ * Class Voyage
+ * @package App\Entity
+ *
+ */
+
 
 class Voyage
 {
+private $idVoyage;
+
+private $idPassager;
+
+private $dateVoyage;
+
+private $typeVoyage;
+
+private $numeroTransport;
+
+private $lieuDepart;
+
+private $lieuArrive;
+
+private $effectue;
 
 }
